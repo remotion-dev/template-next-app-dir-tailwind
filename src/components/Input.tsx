03e6696952860f7f152a5c906d1856b6f07c7f06
JonnyBurger <jonathanburger11@gmail.com>
2026-02-26 17:36:35 +0100

    Update template

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,6 +14,8 @@ export const Input: React.FC<{
 
   return (
     <input
+      type="search"
+      autoComplete="off"
       className="leading-[1.7] block w-full rounded-geist bg-background p-geist-half text-foreground text-sm border border-unfocused-border-color transition-colors duration-150 ease-in-out focus:border-focused-border-color outline-none"
       disabled={disabled}
       name="title"
